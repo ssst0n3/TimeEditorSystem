44b0c52209b86bcd0f889d76627383a5129e2c4f
王三 <ssst0n3@gmail.com>
2016-03-14 13:10:02 +0800

    time_editor

---
+++ src/com/time_editor/auth/AuthController.java
@@ -0,0 +1,12 @@
+package com.time_editor.auth;
+
+import com.jfinal.core.Controller;
+
+public class AuthController extends Controller{
+	public void index() {
+		render("login.html");
+	}
+	public void login() {
+		render("login.html");
+	}
+}
---
+++ src/com/time_editor/index/IndexController.java
@@ -0,0 +1,17 @@
+package com.time_editor.index;
+
+import com.jfinal.core.Controller;
+
+/**
+ * IndexController
+ */
+public class IndexController extends Controller {
+	public void index() {
+		render("login.html");
+	}
+}
+
+
+
+
+
